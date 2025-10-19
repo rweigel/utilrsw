dd8f88fa245d2eda8a748d015b0578679d29d8d9
Bob Weigel <Robert.S.Weigel@gmail.com>
2025-10-19 11:02:04 -0400

    logger

--- utilrsw/logger.py
+++ utilrsw/logger.py
@@ -109,7 +109,7 @@ def logger(name=None,
   def get_filename(log_dir, filename, ext):
 
     if os.path.isabs(filename):
-      return filename
+      return filename + ext
 
     frame = inspect.stack()[-1]
     if log_dir is None:
@@ -133,9 +133,12 @@ def logger(name=None,
     frame = inspect.stack()[1]
     module = inspect.getmodule(frame[0])
     if module and hasattr(module, '__file__'):
-      name = os.path.basename(module.__file__)
+      name = os.path.splitext(os.path.basename(module.__file__))[0]
     else:
       name = '__main__'
+
+  if file_log is None:
+    file_log = name
 
   file_log = get_filename(log_dir, file_log, ".log")
   if file_error is not False:
