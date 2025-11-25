9e1e3d03229685ce644e1106ee366df9d0a46980
Bob Weigel <Robert.S.Weigel@gmail.com>
2025-11-25 15:40:01 -0500

    minimal xprint install

--- setup.py
+++ setup.py
@@ -13,8 +13,25 @@ install_requires = [
     "xmltodict",
     "pyyaml",
     "lxml",
-    "typeguard"
+    "typeguard",
 ]
+
+# Make the default install include the full set of dependencies. Provide a
+# "minimal" extra to allow installing with no dependencies when desired.
+
+# Installs all normal deps
+#   pip install utilrsw
+#   pip install utilrsw[full]
+# Installs no extra deps
+#   pip install utilrsw[minimal]
+# Install only deps for xprint
+#   pip install utilrsw[xprint]
+
+extras_require = {
+    "full": install_requires,
+    "minimal": [],
+    "xprint": []
+}
 
 setup(
     name='utilrsw',
@@ -26,5 +43,6 @@ setup(
     description='Misc utility functions.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    install_requires=install_requires
+    install_requires=install_requires,
+    extras_require=extras_require,
 )
