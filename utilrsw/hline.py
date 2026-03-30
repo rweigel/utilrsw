870a54ddc91ce4d4732e13c642d649c9eee03364
Bob Weigel <Robert.S.Weigel@gmail.com>
2026-03-30 10:12:29 -0400

    update hline.py

--- utilrsw/hline.py
+++ utilrsw/hline.py
@@ -1,7 +1,10 @@
-def hline(fallback=(80, 24), char="-", display=True):
+def hline(fallback=(80, 24), char="-", indent=0, display=True):
   import shutil
-  line = char * shutil.get_terminal_size(fallback=fallback).columns
+  if isinstance(indent, int):
+    indent = " " * indent
+  columns = shutil.get_terminal_size(fallback=fallback).columns - len(indent)
+  line = char * columns 
   if display:
-    print(line)
+    print(f"{indent}{line}")
   else:
-    return line
+    return f"{indent}{line}"
