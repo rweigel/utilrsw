9f3f06a203d3a0c2e8a9124f0fc2f665280ce8c3
Bob Weigel <rweigel@gmu.edu>
2026-06-17 21:43:46 -0400

    add exclude_date_metadata to savefig.py

--- utilrsw/mpl/savefig.py
+++ utilrsw/mpl/savefig.py
@@ -1,4 +1,4 @@
-def savefig(fname, fdir='', formats=None, subdirs=False, bbox_inches='tight'):
+def savefig(fname, fdir='', exclude_date_metadata=True, formats=None, subdirs=False, bbox_inches='tight'):
 
   import os
   from matplotlib import pyplot as plt
@@ -19,9 +19,27 @@ def savefig(fname, fdir='', formats=None, subdirs=False, bbox_inches='tight'):
       subdirs = []
 
   for format in formats:
+    rc = {}
     kwargs = {'bbox_inches': bbox_inches}
     if format == 'png':
       kwargs['dpi'] = 300
+
+    """
+    Note: If exclude_date_metadata, don't include metadata in SVG and PDF to
+          avoid non-deterministic content that prevents caching and causes
+          differences in version controlled files
+    """
+    if format == 'svg':
+      kwargs['metadata'] = {"Date": None} # See note above.
+      # svg.fonttype => Don't convert text to paths to keep it searchable and selectable
+      # svg.hashsalt => See note above
+      rc = {'svg.fonttype': 'none'}
+      if exclude_date_metadata:
+        rc['svg.hashsalt'] = '67'
+
+    if format == 'pdf' and exclude_date_metadata:
+      # See note above
+      kwargs['metadata'] = {'CreationDate': None, 'ModDate': None}
 
     if format in subdirs:
       fname_full = os.path.join(fdir, format, f'{fname}.{format}')
@@ -30,6 +48,7 @@ def savefig(fname, fdir='', formats=None, subdirs=False, bbox_inches='tight'):
 
     os.makedirs(os.path.dirname(fname_full), exist_ok=True)
     print(f"  Writing {fname_full}")
-    plt.savefig(fname_full, bbox_inches=bbox_inches)
+    with plt.rc_context(rc):
+      plt.savefig(fname_full, **kwargs)
 
   plt.close()
